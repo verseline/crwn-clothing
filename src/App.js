8762c706b0a3439307245a3bd1e7a19f595157e5
verseline <verselinelist@gmail.com>
2022-04-05 22:19:37 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div className='app'>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
